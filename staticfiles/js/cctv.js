3ac82cad7eeb49e9bdfaa679284d8531e9a5cd84
seokcode <ssd3784@gmail.com>
2023-05-18 16:11:50 +0900

    [UPDATE] requirements.txt 수정, staticfiles 추가

--- staticfiles/js/cctv.js
+++ staticfiles/js/cctv.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const video = document.getElementById("video");
+function zoomVideo(video) {
+    if (!video.classList.contains('video-zoomed')) {
+        // 다른 비디오의 확대를 중지하고 중앙에 위치시킵니다.
+        const videos = document.querySelectorAll('.video-container video');
+        videos.forEach((v) => {
+            if (v !== video) {
+                v.classList.remove('video-zoomed');
+            }
+        });
 
-    video.addEventListener("canplay", () => {
-        video.play();
-    });
-
-    // Handle video playback errors
-    video.addEventListener("error", () => {
-        alert("An error occurred while playing the video.");
-    });
-});
+        video.classList.add('video-zoomed');
+    } else {
+        video.classList.remove('video-zoomed');
+    }
+}
